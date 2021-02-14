01698c6ff02e02202a634dba357cedbbd2fcee33
ALIHAN <ALIHAN@DESKTOP-52972LB>
2021-02-14 21:18:16 +0300

    3.2.2021

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -15,7 +15,7 @@ namespace Entities.Concrete
 
         public short UnitsInStock { get; set; }
 
-        public decimal UnitPrice { get; set; }//urun birim fiyatı
+        public decimal UnitPrice { get; set; }//urun birim fiyatı 
 
     }
 }
