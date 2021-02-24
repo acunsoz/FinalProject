1a6a97b9c9053b848a43f64a7e3258b141f4a30c
ALIHAN <ALIHAN@DESKTOP-52972LB>
2021-02-25 00:10:15 +0300

    24 subat ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,9 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi gecersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductsListed="Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Ekleme yapılamaz en fazla 10 urun";
+        public static string ProductNameAlreadyExists = "Bu isimde baska urun var";
+        public static string CategoryLimitExceded="Kategory limiti asıldıgı icin yeni urun eklenemiyor";
     }
 }
